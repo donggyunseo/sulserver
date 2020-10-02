f4b73f187678186ec4345c0fb0cfec00df7ce3d2
Seo Donggyun <seodonggyun7@gmail.com>
2020-10-02 23:02:16 +0900

    :(

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/reviewList', async (req, res, next) => {
   try {
     const connection = await pool.getConnection()
     const [ reviews ] = await connection.query('SELECT * FROM REVIEWS')
-    res.header("Access-Control-Allow-Origin", "*")
+    req.header("Access-Control-Allow-Origin", "*")
     res.json({ status: 200, arr: reviews })
 
     connection.release()
