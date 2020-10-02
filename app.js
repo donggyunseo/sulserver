3e4ebec91b97503adfe65fef8d228c67bf59bf66
Seo Donggyun <seodonggyun7@gmail.com>
2020-10-02 23:00:24 +0900

    쫌 돼라

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,8 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
